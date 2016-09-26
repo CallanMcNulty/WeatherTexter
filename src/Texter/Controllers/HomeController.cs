718984660a1a94b4d189586fe7541aeda3a5fbce
Callan McNulty <jabberwocky222@gmail.com>
2016-09-26 16:46:31 -0700

    Add local storage of contacts, alternatate message types

--- src/Texter/Controllers/HomeController.cs
+++ src/Texter/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Texter.Models;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace Texter.Controllers
 {
@@ -28,7 +31,23 @@ namespace Texter.Controllers
         [HttpPost]
         public IActionResult SendMessage(Message newMessage)
         {
-            newMessage.Body = "It's always "+GetWeather(newMessage.Body)+ " in " + newMessage.Body;
+            newMessage.From = EnvironmentVariables.userPhone;
+            if(newMessage.Body.ToLower() == "cat")
+            {
+                newMessage.Body = GetCatFact();
+            }
+            else if (newMessage.Body.ToUpper() == "BILL MURRAY")
+            {
+                newMessage.Body = GetBillBio();
+            }
+            else if (newMessage.Body.ToUpper() == "CHUCK NORRIS")
+            {
+                newMessage.Body = GetRoundhouseKickedInTheFace();
+            }
+            else
+            {
+                newMessage.Body = "It's always "+GetWeather(newMessage.Body)+ " in " + newMessage.Body;
+            }
             ViewData["NumberSentTo"] = newMessage.To;
             newMessage.Send();
             return View("Index");
@@ -38,6 +57,57 @@ namespace Texter.Controllers
         {
             string weatherData = Weather.GetCityWeather(City);
             return weatherData;
+        }
+
+        public string GetCatFact()
+        {
+            var client = new RestClient("http://catfacts-api.appspot.com/api");
+            var request = new RestRequest("facts", Method.GET);
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            string fact = jsonResponse["facts"][0].ToString();
+            return fact;
+        }
+
+        public string GetBillBio()
+        {
+            var client = new RestClient("https://en.wikipedia.org/w/");
+            var request = new RestRequest("api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=Bill%20Murray", Method.GET);
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            string fact = jsonResponse["query"]["pages"]["102994"]["extract"].ToString();
+            return fact;
+        }
+
+        public string GetRoundhouseKickedInTheFace()
+        {
+            var client = new RestClient("http://api.icndb.com/");
+            var request = new RestRequest("jokes/random", Method.GET);
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            string fact = jsonResponse["value"]["joke"].ToString();
+            return fact;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
         }
     }
 }
--- src/Texter/wwwroot/js/userStorage.js
+++ src/Texter/wwwroot/js/userStorage.js
@@ -1,10 +1,33 @@
-﻿var recipientInput = document.getElementsByName('To');
-var recipientLabel = document.querySelectorAll('[for="To"]');
+﻿var recipientInput = document.getElementsByName('To')[0];
+var recipientLabel = document.querySelectorAll('[for="To"]')[0];
 var dropdownMenu = document.getElementById('dropdownMenu');
+var html = "<ul>";
+var contacts = JSON.parse(localStorage.getItem("contacts"));
+var keys = Object.keys(contacts);
+for (var i = 0; i < keys.length; i++) {
+    html += "<li class='contact-list-item' id='" + keys[i] + "'>" + contacts[keys[i]] + "</li>";
+}
+html += "</ul>";
+dropdownMenu.innerHTML = html;
 
-recipientInput[0].addEventListener("click", showContactsDropdown);
-recipientLabel[0].addEventListener("click", showContactsDropdown);
+recipientInput.addEventListener("click", showContactsDropdown);
+recipientLabel.addEventListener("click", showContactsDropdown);
 
 function showContactsDropdown() {
-    dropdownMenu.removeAttribute('class');
+    dropdownMenu.className = "";
 }
+
+dropdownMenu.addEventListener("click", function (e) {
+    if (e.target && e.target.matches("li.contact-list-item")) {
+        recipientInput.value = "+" + e.target.id;
+        dropdownMenu.className = "hidden";
+    }
+});
+
+
+///fill city input box with Chuck Norris
+var chuckSpan = document.getElementById('chuck');
+console.log(chuckSpan);
+chuckSpan.addEventListener("click", function (e) {
+    document.getElementsByName("Body")[0].value = "CHUCK NORRIS";
+});
